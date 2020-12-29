28f8e34d9861de79dcb615bc7035914cfafd4a82
Doan Nhat Anh <nhatanhdoan1505@gmail.com>
2020-12-29 14:24:41 +0700

    Updates

---
+++ precache-manifest.361de06449994659ce3e3bdcd4ec54ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ebb31157c1da01811facacfa0883bf",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "5a7e83eada0ec0212e53",
+    "url": "/todo/static/css/main.d57a65c6.chunk.css"
+  },
+  {
+    "revision": "43f927420dcd2216375a",
+    "url": "/todo/static/js/2.d144f112.chunk.js"
+  },
+  {
+    "revision": "6177d1ab9ae3c0c435b4d0c754a1b33b",
+    "url": "/todo/static/js/2.d144f112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a7e83eada0ec0212e53",
+    "url": "/todo/static/js/main.6ef264f5.chunk.js"
+  },
+  {
+    "revision": "4be3134114a828ee75b2",
+    "url": "/todo/static/js/runtime-main.fd899b97.js"
+  },
+  {
+    "revision": "e57071b5ad62f86f4ef617730d281fb3",
+    "url": "/todo/static/media/background.e57071b5.jpg"
+  },
+  {
+    "revision": "b164c8c067dc0bdfe083b2e1452d4db9",
+    "url": "/todo/static/media/image.b164c8c0.jpg"
+  }
+]);
